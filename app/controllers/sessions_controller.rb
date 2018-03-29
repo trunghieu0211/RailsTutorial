cf0a7ae1397a6a13f108c9c95a44c52a3acb72c9
trunghieu0211 <trunghieugx0211@gmail.com>
2018-03-29 17:30:56 +0700

    chap 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash[:success] = t ".success"
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      remember user
       redirect_to user
     else
       flash.now[:danger] = t ".login_fail"
