875a94b9fd6da88c1dcafe56f1a73f0928f4c08a
trunghieu0211 <trunghieugx0211@gmail.com>
2018-03-30 18:02:49 +0700

    chap 12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: t(".activation")
   end
 
-  def password_reset
-    @greeting = t ".hi"
-
-    mail to: t(".url")
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: t(".pass_reset")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20180330080454) do
+ActiveRecord::Schema.define(version: 20180330101615) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -11,6 +11,8 @@ ActiveRecord::Schema.define(version: 20180330080454) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
