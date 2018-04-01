aa217f3bc2e0e38a0bfcf609532e3c1565566638
trunghieu0211 <trunghieugx0211@gmail.com>
2018-04-01 20:42:19 +0700

    chap 14

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,13 +6,4 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true,
     length: {maximum: Settings.micropost.content.max_length}
-  validates  :picture_size
-
-  private
-
-  def picture_size
-    if picture.size > Settings.micropost.max_picture_size.megabytes
-      errors.add :picture, t(".max_picture_size")
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name,
+    foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+    foreign_key: :followed_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   before_save {email.downcase!}
   before_create :create_activation_digest
@@ -37,7 +43,7 @@ class User < ApplicationRecord
     update_attributes remember_digest: User.digest(remember_token)
   end
 
-  def authenticated? attribute, token
+  def authenticated?(attribute, token)
     digest = send "#{attribute}_digest"
 
     return false if digest.nil?
@@ -72,6 +78,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.load_feed id
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   private
